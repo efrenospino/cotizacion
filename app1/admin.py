dcf37361db835bc3c452f53100345a0cc5ee3f3e
efrenospino <efrenospino@outlook.com>
2015-09-29 17:35:56 -0500

    Revert "Sistema de cotizaciones"
    This reverts commit 81f9aeade252db64be698743901d00a5d71e57e1.

--- app1/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from app1.models import *
-
-admin.site.register(Producto)
--- app1/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Producto(models.Model):
-	nombre = models.CharField(max_length=100)
-	descripcion = models.CharField(max_length=255, blank=True)
-	idUnidad = models.IntegerField()
-	precio = models.FloatField()
-	eliminado = models.BooleanField(default=False)
