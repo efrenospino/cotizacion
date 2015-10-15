47bd93f33df1ed2948760c9aa9300145af2fb25a
Efren Ospino <efrenospino@outlook.com>
2015-10-14 20:41:56 -0500

    list_display
    Mostrando información en la tabla de cada modelo

--- app1/admin.py
+++ app1/admin.py
@@ -2,13 +2,28 @@ from django.contrib import admin
 from app1.models import *
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display=('id','identificacion','razonSocial','nombres','apellidos')
+    list_display = ('id','identificacion','razonSocial','nombres','apellidos')
     
-admin.site.register(Producto)
-admin.site.register(Servicio)
-admin.site.register(Cotizacion)
-admin.site.register(DetalleCotizacion)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('id','identificacion','razonSocial','direccion')
+    
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre','descripcion','precio', 'idUnidad')
+    
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre','descripcion','precio')
+
+class CotizacionAdmin(admin.ModelAdmin):
+    list_display = ('id','fecha','cliente','total')
+    
+class DetalleCotizacionAdmin(admin.ModelAdmin):
+    list_display = ('id','cotizacion','producto','servicio', 'cantidad', 'subtotal')
+    
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Servicio, ServicioAdmin)
+admin.site.register(Cotizacion, CotizacionAdmin)
+admin.site.register(DetalleCotizacion, DetalleCotizacionAdmin)
 admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Empresa)
+admin.site.register(Empresa, EmpresaAdmin)
 admin.site.register(Parametro)
 admin.site.register(ValorParametro)
